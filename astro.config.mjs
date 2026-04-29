52e655fe2c80b709fc7f11265e87460966b92504
venclto23 <venclicek.tom@gmail.com>
2026-04-29 18:06:12 +0200

    ikonky

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,25 +2,26 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 import keystatic from '@keystatic/astro';
-import netlify from '@astrojs/netlify';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  // --- ZMĚNĚNO Z HYBRID NA STATIC ---
+  site: 'https://slapy-hrdlicka.cz',
   output: 'static',
-  adapter: netlify(),
-  // ----------------------------------
 
   server: {
     port: 3000,
   },
+
   vite: {
     plugins: [tailwindcss()],
   },
+
   integrations: [
     react(),
     keystatic({
       adminPath: '/admin',
       configFile: './keystatic.config.ts',
     }),
+    sitemap(),
   ],
 });
