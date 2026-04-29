9c5d1c3564a5635187ef83827b91c4eb91d15d20
venclto23 <venclicek.tom@gmail.com>
2026-04-29 18:22:07 +0200

    adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,12 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 import keystatic from '@keystatic/astro';
 import sitemap from '@astrojs/sitemap';
+import netlify from '@astrojs/netlify'; // 1. TENTO IMPORT TI CHYBĚL
 
 export default defineConfig({
   site: 'https://slapy-hrdlicka.cz',
   output: 'static',
+  adapter: netlify(), // 2. TENTO ŘÁDEK TI CHYBĚL
 
   server: {
     port: 3000,
